d891c1f47b7fbb7416fec8f815464b3bf5f3d3d6
Администратор <@comp>
2016-12-15 22:51:07 +0300

    3+

--- src/service/ServiceFactory.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import exception.PersistentException;
-
-public interface ServiceFactory {
-	<Type extends Service> Type getService(Class<Type> key) throws PersistentException;
-
-	void close();
-}
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -5,14 +5,18 @@ import java.util.List;
 import domain.User;
 import exception.PersistentException;
 
-public interface UserService extends Service {
-	List<User> findAll() throws PersistentException;
 
-	User findById(Integer id) throws PersistentException;
+public interface UserService {
+    List<User> findAll() throws PersistentException;
+    
+    
+    User findById(Integer id) throws PersistentException;//читает пользователя по id
+    
+    
+    
+    void save(User user) throws  PersistentException;//сохраняет нового пользователя
+    void delete(Integer id) throws PersistentException;//удаляет пользователя
+    User readUser(String login, String password) throws PersistentException ;//чтение пользователя по логину и паролю
+    boolean chechUnique(String login) throws PersistentException;// проверяет уникальность логина,возвращает true,если логин уникальный
 
-	User findByLoginAndPassword(String login, String password) throws PersistentException;
-
-	void save(User user) throws PersistentException;
-
-	void delete(Integer id) throws PersistentException;
 }
